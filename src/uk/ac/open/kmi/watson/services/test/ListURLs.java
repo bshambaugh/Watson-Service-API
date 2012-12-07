dc89005de3eed2bf95e17a67192d6ec1384f9e57
mdaquin <mathieu@daquin.net>
2012-12-07 21:04:48 +0000

    clean up and test

--- src/uk/ac/open/kmi/watson/services/test/ListURLs.java
+++ src/uk/ac/open/kmi/watson/services/test/ListURLs.java
@@ -6,30 +6,17 @@ import java.util.Vector;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.search.Searcher;
 
-import uk.ac.open.kmi.watson.services.WatsonService;
+import uk.ac.open.kmi.watson.services.*;
 
-public class ListURLs extends WatsonService {
+public class ListURLs extends SemanticContentSearch {
 
-	public void listURLs(){
-		String[] result = null;
-		try {
-			Searcher ir = getDocumentIndexSearcher();
-			for (int i = 0; i < ir.maxDoc(); i++) {
-				Document d = ir.doc(i);
-				String urls = d.get("provs");
-				StringTokenizer t = new StringTokenizer(urls);
-				while (t.hasMoreTokens()) System.out.println(t.nextToken());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    public static void main(String[] args){
+	ListURLs lu = new ListURLs();
+	String[] res = lu.listSemanticContents(0,100);
+	for (String r : res){
+	    System.out.println(r);
 	}
-
-	
-	public static void main(String[] args){
-		ListURLs lu = new ListURLs();
-		lu.listURLs();
-	}
+    }
 	
 	
 }
